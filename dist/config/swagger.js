6db7ec1cbc6256b9793f9be540f626772319097c
quangvm <quangvm@hqsoft.com.vn>
2025-03-24 00:46:46 +0700

    comment swagger

--- dist/config/swagger.js
+++ dist/config/swagger.js
@@ -1,26 +1,21 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.setupSwagger = void 0;
-const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
-const swagger_jsdoc_1 = __importDefault(require("swagger-jsdoc"));
-const path_1 = __importDefault(require("path"));
-const swaggerOptions = {
-    definition: {
-        openapi: "3.0.0",
-        info: {
-            title: "Cooking Meal Service API",
-            version: "1.0.0",
-            description: "A simple API for managing meals",
-        },
-    },
-    apis: [path_1.default.join(__dirname, "../routes/*.js")], // Use .ts for TypeScript
-};
-const swaggerDocs = (0, swagger_jsdoc_1.default)(swaggerOptions);
-const setupSwagger = (app) => {
-    app.use("/swagger", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swaggerDocs));
-};
-exports.setupSwagger = setupSwagger;
+// import swaggerUi from "swagger-ui-express";
+// import swaggerJsdoc from "swagger-jsdoc";
+// import { Application } from "express";
+// import path from "path";
+// const swaggerOptions = {
+//     definition: {
+//         openapi: "3.0.0",
+//         info: {
+//             title: "Cooking Meal Service API",
+//             version: "1.0.0",
+//             description: "A simple API for managing meals",
+//         },
+//     },
+//     apis: [path.join(__dirname, "../routes/*.js")], // Use .ts for TypeScript
+// };
+// const swaggerDocs = swaggerJsdoc(swaggerOptions);
+// export const setupSwagger = (app: Application) => {
+//     app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+// };
 //# sourceMappingURL=swagger.js.map
--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -1,23 +1,23 @@
-import swaggerUi from "swagger-ui-express";
-import swaggerJsdoc from "swagger-jsdoc";
-import { Application } from "express";
-import path from "path";
+// import swaggerUi from "swagger-ui-express";
+// import swaggerJsdoc from "swagger-jsdoc";
+// import { Application } from "express";
+// import path from "path";
 
-const swaggerOptions = {
-    definition: {
-        openapi: "3.0.0",
-        info: {
-            title: "Cooking Meal Service API",
-            version: "1.0.0",
-            description: "A simple API for managing meals",
-        },
-    },
-    apis: [path.join(__dirname, "../routes/*.js")], // Use .ts for TypeScript
-};
+// const swaggerOptions = {
+//     definition: {
+//         openapi: "3.0.0",
+//         info: {
+//             title: "Cooking Meal Service API",
+//             version: "1.0.0",
+//             description: "A simple API for managing meals",
+//         },
+//     },
+//     apis: [path.join(__dirname, "../routes/*.js")], // Use .ts for TypeScript
+// };
 
 
-const swaggerDocs = swaggerJsdoc(swaggerOptions);
+// const swaggerDocs = swaggerJsdoc(swaggerOptions);
 
-export const setupSwagger = (app: Application) => {
-    app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-};
+// export const setupSwagger = (app: Application) => {
+//     app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+// };
